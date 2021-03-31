e8b0b566b9404a6182ada3e0e3fd523b3ee80634
Jason Lin <jasonlin1198@gmail.com>
2021-03-31 19:21:59 -0400

    plugin menu

--- netbox_qrcode/view.py
+++ netbox_qrcode/view.py
@@ -1,2 +1,8 @@
-# class SiteTopologyView(TopologyView):
-#     template_name = 'nextbox_ui_plugin/site_topology.html'
+from django.shortcuts import get_object_or_404, render
+from django.views import View
+
+class QRcodeHomeView(View):
+
+    def get(self, request):
+
+        return render(request,"netbox_qrcode/home.html")
