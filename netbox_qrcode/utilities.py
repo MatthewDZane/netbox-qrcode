30bf9ebb4785068b3297d664288986dc89315a5c
Jason Lin <jasonlin1198@gmail.com>
2021-03-31 01:20:32 -0400

    test font

--- netbox_qrcode/utilities.py
+++ netbox_qrcode/utilities.py
@@ -39,12 +39,12 @@ def get_qr_text(size, text, font='ArialMT'):
         #file = open(r'/opt/netbox/venv/lib/python3.8/site-packages/netbox_qrcode/fonts/TahomaBold.ttf', "rb")
         #bytes_font = BytesIO(file.read())
         try:
-            fnt = ImageFont.truetype(os.path.join(os.path.dirname(__file__), 'tahomatest.ttf'),font_size)
+            fnt = ImageFont.truetype(os.path.join(os.path.dirname(__file__), 'fonts/tahomatest.ttf'),font_size)
             font_success = 'Success'
         except Exception:
             fnt = ImageFont.load_default()
             flag = False
-            font_success = 'Failed getting ' + os.path.join(os.path.dirname(__file__), 'tahomatest.ttf')
+            font_success = 'Failed getting ' + os.path.join(os.path.dirname(__file__), 'fonts/tahomatest.ttf')
 
         draw = ImageDraw.Draw(img)
         w, h = draw.textsize(text, font=fnt)
@@ -54,7 +54,7 @@ def get_qr_text(size, text, font='ArialMT'):
     W, H = size
     draw.text(((W-w)/2, (H-h)/2), text, font=fnt, fill='black')
 
-    files = os.listdir('/opt/netbox/venv/lib/python3.8/site-packages/netbox_qrcode/') + os.listdir('/opt/netbox/venv/lib/python3.8/site-packages/netbox_qrcode/fonts') + os.listdir('/opt/netbox/venv/lib/python3.8/site-packages/netbox_qrcode/templates') 
+    files = os.listdir('/opt/netbox/venv/lib/python3.8/site-packages/netbox_qrcode/') + os.listdir('/opt/netbox/venv/lib/python3.8/site-packages/netbox_qrcode/fonts') + os.listdir('/opt/netbox/venv/lib/python3.8/site-packages/netbox_qrcode/templates/netbox_qrcode') 
     return img,font_success, files
 
 
