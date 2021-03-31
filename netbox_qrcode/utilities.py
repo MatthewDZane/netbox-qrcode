f1d280d8807066cdbac679a104aef22f6480eab6
Jason Lin <jasonlin1198@gmail.com>
2021-03-30 20:53:13 -0400

    test font

--- netbox_qrcode/utilities.py
+++ netbox_qrcode/utilities.py
@@ -32,13 +32,17 @@ def get_qr_text(size, text, font='ArialMT'):
     img = Image.new('L', size, 'white')
     font_size = 100
     flag = True
+    font_success = ''
     while flag:
-        file_path = resource_stream(__name__, 'fonts/{}.ttf'.format(font))
+        #file_path = resource_stream(__name__, 'fonts/{}.ttf'.format(font))
         try:
-            fnt = ImageFont.truetype("Ubuntu-R.ttf", font_size)
+            fnt = ImageFont.truetype('arial.ttf', font_size)
+            font_success = 'Success'
         except Exception:
             fnt = ImageFont.load_default()
             flag = False
+            font_success = 'Failed'
+
         draw = ImageDraw.Draw(img)
         w, h = draw.textsize(text, font=fnt)
         if w < size[0] - 4 and h < size[1] - 4:
@@ -46,7 +50,7 @@ def get_qr_text(size, text, font='ArialMT'):
         font_size -= 1
     W, H = size
     draw.text(((W-w)/2, (H-h)/2), text, font=fnt, fill='black')
-    return img
+    return img,font_success
 
 
 def get_concat(im1, im2):
