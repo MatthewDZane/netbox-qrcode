60c334bf4f58b4cd79ba840e3f76ead8e713acf5
Jason Lin <jasonlin1198@gmail.com>
2021-03-30 20:40:33 -0400

    test font

--- netbox_qrcode/utilities.py
+++ netbox_qrcode/utilities.py
@@ -35,7 +35,7 @@ def get_qr_text(size, text, font='ArialMT'):
     while flag:
         file_path = resource_stream(__name__, 'fonts/{}.ttf'.format(font))
         try:
-            fnt = ImageFont.truetype('/opt/netbox/venv/lib/python3.8/site-packages/netbox_qrcode/fonts/ComicSansMSBold.ttf', font_size)
+            fnt = ImageFont.truetype(r'/opt/netbox/venv/lib/python3.8/site-packages/netbox_qrcode/fonts/ArialBlack.ttf', font_size)
         except Exception:
             fnt = ImageFont.load_default()
             flag = False
