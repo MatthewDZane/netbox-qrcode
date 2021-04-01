411b89cdc97f4d0d231f2e6cfc276c46433ae162
Jason Lin <jasonlin1198@gmail.com>
2021-03-31 22:36:56 -0400

    attempt to include search panel

--- netbox_qrcode/navigation.py
+++ netbox_qrcode/navigation.py
@@ -2,7 +2,7 @@ from extras.plugins import PluginMenuItem
 
 menu_items = (
     PluginMenuItem(
-        link='plugins:netbox_qrcode:qrcodes',
+        link='plugins:netbox_qrcode:QRCodes',
         link_text='Netbox QRcode',
         buttons=()
     ),
--- netbox_qrcode/views.py
+++ netbox_qrcode/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import get_object_or_404, render
 from django.views import View
+from dcim.models import Cable, Device, Interface, DeviceRole
+from . import forms, filters
+
+# class QRcodeHomeView(View):
+
+#     def get(self, request):
+#          return render(request,"netbox_qrcode/home.html")
 
 class QRcodeHomeView(View):
+    queryset = Device.objects.all()
+    filterset = filters.SearchFilterSet
+    template_name = 'netbox_qrcode/home.html'
 
     def get(self, request):
-         return render(request,"netbox_qrcode/home.html")
+
+        if not request.GET:
+            self.queryset = Device.objects.none()
+
+        layout_context = {}
+
+        self.queryset = self.filterset(request.GET, self.queryset).qs
+
+        return render(request, self.template_name)
