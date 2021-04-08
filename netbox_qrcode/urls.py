b284667e099baa1f27797e7c1338c6d0dd8e8671
Jason Lin <jasonlin1198@gmail.com>
2021-04-08 15:47:06 -0400

    remove printview

--- netbox_qrcode/urls.py
+++ netbox_qrcode/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.QRcodeHomeView.as_view(), name='qrcode_menu'),
-    path('print/', views.PrintView.as_view(), name='print_menu'),
+    # path('print/', views.PrintView.as_view(), name='print_menu'),
 
 ]
