5604bec289ae5a21d92d63d6ef591e1cbbacef59
Jason Lin <jasonlin1198@gmail.com>
2021-04-13 21:37:35 -0400

    check resource_stream path

--- netbox_qrcode/utilities.py
+++ netbox_qrcode/utilities.py
@@ -36,6 +36,7 @@ def get_qr_text(size, text, font='ArialMT'):
     while flag:
         file_path = resource_stream(__name__, 'fonts/{}.ttf'.format(font))
         path = __name__
+        package = __package__
         try:
             fnt = ImageFont.truetype(file_path,font_size)
         except Exception:
@@ -50,7 +51,7 @@ def get_qr_text(size, text, font='ArialMT'):
     W, H = size
     draw.text(((W-w)/2, (H-h)/2), text, font=fnt, fill='black')
 
-    return img, path
+    return img, path, package
 
 
 def get_concat(im1, im2):
