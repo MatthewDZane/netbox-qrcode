c516fb013fd8c190d887fa456142583daa3e31a4
Jason Lin <jasonlin1198@gmail.com>
2021-04-14 20:32:56 -0400

    separate views

--- netbox_qrcode/navigation.py
+++ netbox_qrcode/navigation.py
@@ -2,7 +2,7 @@ from extras.plugins import PluginMenuItem
 
 menu_items = (
     PluginMenuItem(
-        link='plugins:netbox_qrcode_ui:qrcode_devices',
+        link='plugins:netbox_qrcode:qrcode_devices',
         link_text='QR Codes',
         buttons=()
     ),
