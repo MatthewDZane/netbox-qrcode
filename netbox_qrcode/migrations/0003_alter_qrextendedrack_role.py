ba2f38fbaeae0a3c45f19acb13480e01e668564a
Jason Lin <jasonlin1198@gmail.com>
2021-04-19 21:00:40 -0400

    add migrations

--- netbox_qrcode/migrations/0003_alter_qrextendedrack_role.py
+++ netbox_qrcode/migrations/0003_alter_qrextendedrack_role.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dcim', '0130_sitegroup'),
+        ('dcim', '0122_standardize_name_length'),
         ('netbox_qrcode', '0002_auto_20210420_0042'),
     ]
 
