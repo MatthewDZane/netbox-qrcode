1dbf1cb0ba9c368962660c70dc564fa0c6f447ec
Matthew Zane <Matthewdzane@gmail.com>
2022-08-10 23:52:44 -0700

    Template content will display previously generated qr codes

--- netbox_qrcode/template_content.py
+++ netbox_qrcode/template_content.py
@@ -14,53 +14,17 @@ class QRCode(PluginTemplateExtension):
         obj = self.context['object']
         request = self.context['request']
         url = request.build_absolute_uri(obj.get_absolute_url())
-        # Get object config settings
-        obj_cfg = config.get(self.model.replace('dcim.', ''))
-        if obj_cfg is None:
-            return ''
-        # and override default config
-        config.update(obj_cfg)
-
-        qr_args = {}
-        for k, v in config.items():
-            if k.startswith('qr_'):
-                qr_args[k.replace('qr_', '')] = v
-
-        # Create qr image
-        qr_img = get_qr(url, **qr_args)
 
         # Handle qr text if enabled
         if config.get('with_text'):
-            text = []
-            for text_field in config.get('text_fields', []):
-                cfn = None
-                if '.' in text_field:
-                    try:
-                        text_field, cfn = text_field.split('.')
-                    except ValueError:
-                        cfn = None
-                if getattr(obj, text_field, None):
-                    if cfn:
-                        try:
-                            if getattr(obj, text_field).get(cfn):
-                                text.append('{}'.format(getattr(obj, text_field).get(cfn)))
-                        except AttributeError:
-                            pass
-                    else:
-                        text.append('{}'.format(getattr(obj, text_field)))
-            custom_text = config.get('custom_text')
-            if custom_text:
-                text.append(custom_text)
-            text = '\n'.join(text)
-
-            # Create qr text with image size and text
-            text_img = get_qr_text(qr_img.size, text, config.get('font'))
-            qr_with_text = get_concat(qr_img, text_img, config.get('text_location', 'right'))
-
-            img = get_img_b64(qr_with_text)
+            base_url = request.build_absolute_uri('/') + 'media/image-attachments/'
+            with_text_url = '{}{}.png'.format(base_url, obj._meta.object_name + str(obj.pk))
+            img = with_text_url
             
         else:
-            img = get_img_b64(qr_img)
+            base_url = request.build_absolute_uri('/') + 'media/image-attachments/'
+            no_text_url = '{}noText{}.png'.format(base_url, obj._meta.object_name + str(obj.pk))
+            img = no_text_url
         try:
             if version.parse(settings.VERSION).major >= 3:
                 return self.render(
@@ -95,4 +59,11 @@ class CableQRCode(QRCode):
         return self.x_page()
 
 
-template_extensions = [DeviceQRCode, RackQRCode, CableQRCode]
+class LocationQRCode(QRCode):
+    model = 'dcim.location'
+
+    def right_page(self):
+        return self.x_page()
+
+
+template_extensions = [DeviceQRCode, RackQRCode, CableQRCode, LocationQRCode]
