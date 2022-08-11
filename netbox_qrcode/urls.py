58d34e106a5a169b18073680e518f65c7198212a
Matthew Zane <Matthewdzane@gmail.com>
2022-08-10 23:41:42 -0700

    Added location page to urls

--- netbox_qrcode/urls.py
+++ netbox_qrcode/urls.py
@@ -5,8 +5,10 @@ urlpatterns = [
     path('devices/', views.QRcodeDeviceView.as_view(), name='qrcode_devices'),
     path('racks/', views.QRcodeRackView.as_view(), name='qrcode_racks'),
     path('cables/', views.QRcodeCableView.as_view(), name='qrcode_cables'),
+    path('locations/', views.QRcodeLocationView.as_view(), name='qrcode_locations'),
 
     path('devices/print/', views.PrintView.as_view(), name='print_menu'),
     path('racks/print/', views.PrintView.as_view(), name='print_menu'),
     path('cables/print/', views.PrintView.as_view(), name='print_menu'),
+    path('locations/print/', views.PrintView.as_view(), name='print_menu'),
 ]
